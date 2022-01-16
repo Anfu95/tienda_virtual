bc4eb0f2adfed1c6597dc9c6282729cec117983b
Antec95 <angeelfm18@gmail.com>
2022-01-16 03:12:10 -0600

    ADD-#1-start_project

---
+++ Config/Config.php
@@ -0,0 +1,20 @@
+<?php
+
+const BASE_URL = "http://localhost:8080/tienda_virtual";
+
+//Zona horaria
+date_default_timezone_set("America/Guatemala");
+
+//Datos de conexion
+const DB_HOST = "localhost";
+const DB_NAME = "tienda1";
+const DB_USER = "root";
+const DB_PASSWORD = "";
+const DB_CHARSET = "charset=utf8";
+
+//Deliminadores decimal y millar Ej. 24,1989.00
+const SPD = ",";
+const SPM = ".";
+
+//Simbolo de moneda
+const SMONEY = "$";
